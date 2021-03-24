ddd625faac706d56cef8c9b9bdfaeb4f901fa07b
Sergey7235 <yshkoff-serg@yandex.com>
2021-03-24 23:54:43 +0500

    E-Soft

---
+++ Registration_and_Login_Forms/Log_in.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sergio Web Lab Digital Programming Company and Incorporations  </title>
+    <link rel="stylesheet" href="style_1.css"> 
+   <!--  <link rel="icon" type="image" href="https://sun1-24.userapi.com/s/v1/if2/BOK2xOdcXsPRTsiFbX2OYUgymmh3IsuNcQ0Fqyp80Shnx5jThLGjn7zDxNbYrHQ28L708I3Xcphaq9PW0Peh8Ti-.jpg?size=200x0&quality=96&crop=1025,38,686,686&ava=1"> -->
+   <link rel="shortcut icon" href="pin_ico.png" type="image/png">
+</head>
+
+<body>
+ <!-- Логин панель -->
+<center>
+    <section class="flex-container">
+      <div class="card">        
+        <h3 class="log-in">Войдите для авторизации</h3>
+        <input type="login" class="place" placeholder="Имя пользователя" aria-label="Пароль">
+        <input type="password" class="place" placeholder="Пароль" aria-label="Пароль">
+        
+      </div>
+
+    </section>
+</center>
+
+</body>
+</html>
---
+++ Registration_and_Login_Forms/Registration.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sergio Web Lab Digital Programming Company and Incorporations  </title>
+    <link rel="stylesheet" href="style_1.css"> 
+   <!--  <link rel="icon" type="image" href="https://sun1-24.userapi.com/s/v1/if2/BOK2xOdcXsPRTsiFbX2OYUgymmh3IsuNcQ0Fqyp80Shnx5jThLGjn7zDxNbYrHQ28L708I3Xcphaq9PW0Peh8Ti-.jpg?size=200x0&quality=96&crop=1025,38,686,686&ava=1"> -->
+   <link rel="shortcut icon" href="pin_ico.png" type="image/png">
+</head>
+
+<body>
+ <!-- Логин панель -->
+
+    <section class="flex-container">
+      <div class="card">        
+        <h3 class="log-in">Введите свои данные для регистрации</h3>
+        <input type="login" class="place" placeholder="Имя пользователя" aria-label="Пароль">
+        <input type="password" class="place" placeholder="Пароль" aria-label="Пароль">
+
+        <input type="mail" class="place" placeholder
+        ="e-mail" aria-label="Пароль">
+
+        <input type="button" class="my_button" value="Зарегистрироваться" >
+      </div>
+
+    </section>
+
+</body>
+</html>
